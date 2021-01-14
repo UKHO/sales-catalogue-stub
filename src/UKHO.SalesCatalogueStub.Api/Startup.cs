b3270655f87b4c404c450685f06fa878e5858476
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2021-01-14 14:27:24 +0000

    Add healthchecks

--- src/UKHO.SalesCatalogueStub.Api/Startup.cs
+++ src/UKHO.SalesCatalogueStub.Api/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace UKHO.SalesCatalogueStub.Api
 {
@@ -26,6 +19,8 @@ namespace UKHO.SalesCatalogueStub.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +40,11 @@ namespace UKHO.SalesCatalogueStub.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/health");
             });
         }
     }
