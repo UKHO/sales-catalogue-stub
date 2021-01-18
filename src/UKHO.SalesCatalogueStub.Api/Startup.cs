bdca371dfc2194491dcdba524ddf808211ddc198
Tom Ling <19222781+tomling@users.noreply.github.com>
2021-01-18 16:53:45 +0000

    Add Swagger & Create controllers
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Greg Williams <51328760+UKHOGreg@users.noreply.github.com>

--- src/UKHO.SalesCatalogueStub.Api/Startup.cs
+++ src/UKHO.SalesCatalogueStub.Api/Startup.cs
@@ -3,32 +3,105 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.IO;
+using UKHO.SalesCatalogueStub.Api.Filters;
 
 namespace UKHO.SalesCatalogueStub.Api
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <param name="environment"></param>
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            _hostingEnvironment = environment;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddMvc(options =>
+                {
+                    options.InputFormatters.RemoveType<Microsoft.AspNetCore.Mvc.Formatters.SystemTextJsonInputFormatter>();
+                    options.OutputFormatters.RemoveType<Microsoft.AspNetCore.Mvc.Formatters.SystemTextJsonOutputFormatter>();
+                })
+                .AddNewtonsoftJson(opts =>
+                {
+                    opts.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    opts.SerializerSettings.Converters.Add(new StringEnumConverter(new CamelCaseNamingStrategy()));
+                })
+                .AddXmlSerializerFormatters();
+
+            services
+                .AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("1", new OpenApiInfo
+                    {
+                        Version = "1",
+                        Title = "Sales Catalogue Service API Stub",
+                        Description = "Sales Catalogue Service API Stub",
+                        Contact = new OpenApiContact()
+                        {
+                            Name = "Team Tamatoa",
+                            Url = new Uri("https://github.com/UKHO/sales-catalogue-stub")
+                        }
+                    });
+                    c.CustomSchemaIds(type => type.FullName);
+                    c.IncludeXmlComments($"{AppContext.BaseDirectory}{Path.DirectorySeparatorChar}{_hostingEnvironment.ApplicationName}.xml");
+                    // Sets the basePath property in the Swagger document generated
+                    c.DocumentFilter<BasePathFilter>("/v1/productData");
+
+                    // Include DataAnnotation attributes on Controller Action parameters as Swagger validation rules (e.g required, pattern, ..)
+                    // Use [ValidateModelState] on Actions to actually validate it in C# as well!
+                    c.OperationFilter<GeneratePathParamsValidationFilter>();
+                });
+
             services.AddControllers();
 
             services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                //TODO: Enable production exception handling (https://docs.microsoft.com/en-us/aspnet/core/fundamentals/error-handling)
+                app.UseExceptionHandler("/Error");
+
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
@@ -36,6 +109,13 @@ namespace UKHO.SalesCatalogueStub.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/1/swagger.json", "Sales Catalogue Service API Stub");
+
+            });
 
             app.UseEndpoints(endpoints =>
             {
