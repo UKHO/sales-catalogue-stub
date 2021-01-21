0f2e422868608bdcad25f6a213b07777eaa14c06
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2021-01-21 14:31:16 +0000

    Add Key Vault Access Policies to Create Prerequisites script
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Tom <19222781+tomling@users.noreply.github.com>

--- deployment/create-prerequisites.ps1
+++ deployment/create-prerequisites.ps1
@@ -4,7 +4,9 @@ param (
 	[Parameter(Mandatory=$true)] [string] $resourceGroupName,
 	[Parameter(Mandatory=$true)] [string] $storageAccountName,
 	[Parameter(Mandatory=$true)] [string] $storageContainerName,
-	[Parameter(Mandatory=$true)] [string] $keyVaultName
+	[Parameter(Mandatory=$true)] [string] $keyVaultName,
+	[Parameter(Mandatory=$true)] [string] $adminGroupObjectId,
+	[Parameter(Mandatory=$true)] [string[]] $servicePrincipalObjectIds
 )
 
 Select-AzSubscription $subscriptionId
@@ -30,3 +32,9 @@ New-AzKeyVault -Name $keyVaultName `
 			   -ResourceGroupName $resourceGroupName `
 			   -Location $location `
 			   -Sku Standard
+
+Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ResourceGroupName $resourceGroupName -PermissionsToSecrets get, list, set -ObjectId $adminGroupObjectId
+
+$servicePrincipalObjectIds | foreach-object {	
+	Set-AzKeyVaultAccessPolicy -VaultName $keyVaultName -ResourceGroupName $resourceGroupName -PermissionsToSecrets get, list -ObjectId $_
+}
