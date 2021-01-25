b363d38a69e73c9d3ed2880cc3be8f203aeb315a
Gareth Evans <agareth@engineering.ukho.gov.uk>
2021-01-25 15:38:59 +0000

    Added HealthcheckControllerTest.cs
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>
    Co-authored-by: Bonnie Poole <51318811+bonpoole@users.noreply.github.com>

---
+++ src/UKHO.SalesCatalogueStub.Api.IntegrationTests/HealthcheckControllerTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace UKHO.SalesCatalogueStub.Api.IntegrationTests
+{
+    public class HealthcheckControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
