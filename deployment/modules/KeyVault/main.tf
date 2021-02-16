da65410bfce4892a13a9a9ed115dd6c665caee13
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2021-02-16 17:34:27 +0000

    Remove Key Vault lock due to error

--- deployment/modules/KeyVault/main.tf
+++ deployment/modules/KeyVault/main.tf
@@ -16,10 +16,3 @@ resource "azurerm_key_vault" "keyvault" {
     ]
   }
 }
-
-resource "azurerm_management_lock" "keyvaultlock" {
-  name       = "${local.basename}lock"
-  scope      = azurerm_key_vault.keyvault.id
-  lock_level = "CanNotDelete"
-  notes      = "Locked because we do not want to lose configuration values"
-}
