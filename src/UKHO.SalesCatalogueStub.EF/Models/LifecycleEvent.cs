269ffecd905ebc310a8a88bfb2f8ffc3ec8d7806
Greg Williams <51328760+UKHOGreg@users.noreply.github.com>
2021-03-02 14:40:52 +0000

    Fix LifecycleEvent
    Co-authored-by: Samir Hassoun <51328831+SamirHassoun@users.noreply.github.com>

--- src/UKHO.SalesCatalogueStub.EF/Models/LifecycleEvent.cs
+++ src/UKHO.SalesCatalogueStub.EF/Models/LifecycleEvent.cs
@@ -14,18 +14,18 @@ namespace UKHO.SalesCatalogueStub.Api.EF.Models
         public string EventReasonText { get; set; }
 
         [Column("UnitOfSaleID")]
-        public Guid UnitOfSaleId { get; set; }
-
-        public virtual EventType EventType { get; set; }
+        public Guid? UnitOfSaleId { get; set; }
 
         [Column("EventTypeID")]
         public Guid EventTypeId { get; set; }
 
-        public virtual ProductEdition ProductEdition { get; set; }
-
         [Column("EditionID")]
-        public Guid EditionId { get; set; }
+        public Guid? EditionId { get; set; }
 
-        public DateTime LastUpdated { get; set; }
+        public DateTime? LastUpdated { get; set; }
+
+        public virtual EventType EventType { get; set; }
+
+        public virtual ProductEdition ProductEdition { get; set; }
     }
 }
