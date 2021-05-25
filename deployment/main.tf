ea47524a8a23e0813318c73b9b6a5f148b6f4a77
Tom Ling <19222781+tomling@users.noreply.github.com>
2021-05-25 17:28:43 +0100

    Update version of azurerm

--- deployment/main.tf
+++ deployment/main.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.41.0"
+      version = "=2.60.0"
     }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 0.15.4"
   backend "azurerm" {
     container_name = "tfstate"
     key            = "terraform.tfstate"
